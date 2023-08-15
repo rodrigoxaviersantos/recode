db6f981dd03bde27765b03668e3d1a75fce0815e
Rodrigo Xavier dos Santos <rodrigoxaviersantoss@gmail.com>
2023-08-15 02:49:26 +0000

    :package: add files

---
+++ js/if-else.js
@@ -0,0 +1,13 @@
+let usuario1 = "Sofia"
+let emailUsuario1 = "sofia@gmail.com"
+let senhaUsuario1 = "fgt13"
+
+if(usuario1 = "Sofia" && senhaUsuario1 == "fgt13") {
+    console.log("Usuário(a) autenticado com sucesso!")
+
+    
+} else if(emailUsuario1 == "sofia@gmail.com" && senhaUsuario1 == "fgt13") {
+    console.log("Usuario(a) autenticado com sucesso!")
+} else {
+    console("Usuário e/ou senha inválidas.")
+}
