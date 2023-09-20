b894d9e3e381600614fd2d787c10134afdfc4f40
Rodrigo Xavier dos Santos <rodrigoxaviersantoss@gmail.com>
2023-09-20 22:14:54 +0000

    :data: add project react

---
+++ react/my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Teste from './component/Teste';
+
+function App() {
+  return (
+    <>
+    <Teste/>
+    </>
+  );
+}
+export default App;
---
+++ react/my-app/src/component/Teste.js
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Teste() {
+    return (
+      <>
+      <h1 className='App'>My-App</h1>
+      <h5 style={{color: 'red'}}>Este é um app React</h5>
+      </>
+    );
+  }
+  export default Teste;
